21f65a8e96b36018553b153cecf716e69f10859e
Capt-Mehdi <160503154+Capt-Mehdi@users.noreply.github.com>
2024-05-29 13:12:19 +0500

    Add files via upload

---
+++ styles.js
@@ -0,0 +1,38 @@
+// styles.js
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  container: {
+    flex: 1,
+    width: '100%',
+    padding: 20,
+    backgroundColor: 'rgba(255, 255, 255, 0.6)',
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    color: '#333',
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  scrollViewContent: {
+    flexGrow: 1,
+  },
+  classItem: {
+    backgroundColor: '#f7b731',
+    padding: 15,
+    marginBottom: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  classText: {
+    fontSize: 18,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+});
